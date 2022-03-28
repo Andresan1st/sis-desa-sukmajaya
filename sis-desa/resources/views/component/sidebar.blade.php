15c2671f9d504686da357c20735bbde400dc936c
Andresan1st <andresan797@gmail.com>
2022-03-28 17:02:34 +0700

    new

---
+++ sis-desa/resources/views/component/sidebar.blade.php
@@ -0,0 +1,27 @@
+<ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+    <li class=" nav-item"><a class="d-flex align-items-center" href="index.html"><i data-feather="home"></i><span class="menu-title text-truncate" data-i18n="Dashboards">Dashboards</span><span class="badge badge-light-warning badge-pill ml-auto mr-1">2</span></a>
+        <ul class="menu-content">
+            <li><a class="d-flex align-items-center" href="dashboard-analytics.html"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Analytics">Surat Masuk</span></a>
+            </li>
+            <li><a class="d-flex align-items-center" href="dashboard-ecommerce.html"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="eCommerce">Surat Keluar</span></a>
+            </li>
+        </ul>
+    </li>         
+    <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="copy"></i><span class="menu-title text-truncate" data-i18n="Form Elements">Modul Master</span></a>
+        <ul class="menu-content">
+            <li><a class="d-flex align-items-center" href="form-input.html"><i data-feather="users"></i><span class="menu-item text-truncate" data-i18n="Input">Data Pegawai</span></a>
+            </li>
+            <li><a class="d-flex align-items-center" href="form-input-groups.html"><i data-feather="user"></i><span class="menu-item text-truncate" data-i18n="Input Groups">Data Masyarakat</span></a>
+            </li>
+            <li><a class="d-flex align-items-center" href="form-input-groups.html"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Input Groups">Data Jabatan</span></a>
+            </li>
+            <li><a class="d-flex align-items-center" href="form-input-groups.html"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Input Groups">User Role</span></a>
+            </li>
+            <li><a class="d-flex align-items-center" href="form-input-groups.html"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Input Groups">Struktur Organisasi</span></a>
+            </li>
+        </ul>
+    </li>
+    
+    
+   
+</ul>
