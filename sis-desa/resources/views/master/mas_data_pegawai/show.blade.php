49db413ae2240309a6361f1a281b9562ce45d2b2
Andresan1st <andresan797@gmail.com>
2022-04-10 16:15:56 +0700

    data jabatan dan pegawai done

--- sis-desa/resources/views/master/mas_data_pegawai/show.blade.php
+++ sis-desa/resources/views/master/mas_data_pegawai/show.blade.php
@@ -2,15 +2,17 @@
 @section('content')
     <div class="content-header row">
         <div class="content-header-left col-md-9 col-12 mb-2">
-            <div class="col-12" style="margin-top: 30px">
-                <h2 class="content-header-title float-left mb-0">Data Jabatan</h2>
-                <div class="breadcrumb-wrapper">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="/">Home</a>
-                        </li>
-                        <li class="breadcrumb-item active">Kelola Data Jabatan
-                        </li>
-                    </ol>
+            <div class="row breadcrumbs-top">
+                <div class="col-12" style="margin-top: 30px">
+                    <h2 class="content-header-title float-left mb-0">Data Pegawai</h2>
+                    <div class="breadcrumb-wrapper">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/">Home</a>
+                            </li>
+                            <li class="breadcrumb-item active">Kelola Data Pegawai
+                            </li>
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
@@ -26,15 +28,70 @@
                                 <form id="formdata" class="form form-horizontal">
                                     {{ csrf_field() }}
                                     <div class="form-body">
+                                        <input type="text" id="id" value="{{$datapegawai->id}}" class="form-control" name="id" hidden placeholder="Nama Jabatan" />
                                         <div class="row" style="margin-top: 10px">
                                             <div class="col-12">
                                                 <div class="form-group row">
                                                     <div class="col-md-2 col-form-label">
-                                                        <label for="font-weight-bolder">Nama Jabatan<span
+                                                        <label for="font-weight-bolder">Nama Pegawai<span
                                                             class="required">*</span></label></label>
                                                     </div>
                                                     <div class="col-md-4">
-                                                        <input type="text" id="nama_jabatan" value="{{$datajabatan->nama_jabatan}}" class="form-control" name="nama_jabatan" placeholder="Nama Jabatan" disabled />
+                                                        <input diasbled type="text" id="nama" class="form-control" value="{{$datapegawai->nama}}" name="nama" placeholder="Nama Pegawai" />
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-md-2 col-form-label">
+                                                        <label for="font-weight-bolder">Alamat<span
+                                                            class="required">*</span></label></label>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <textarea diasbled class="form-control" id="alamat"  name="alamat"  rows="3" placeholder="Alamat">{{$datapegawai->alamat}}</textarea>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-md-2 col-form-label">
+                                                        <label for="font-weight-bolder">No Telpon<span
+                                                            class="required">*</span></label></label>
+                                                    </div>
+                                                    <div class="col-xl-4 col-md-6 col-sm-12 mb-2">
+                                                        <div class="input-group input-group-merge">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text">ID (+62)</span>
+                                                            </div>
+                                                            <input diasbled type="text" class="form-control phone-number-mask" name="no_telp" value="{{$datapegawai->no_telp}}"  placeholder="62 877 982 577 86" id="no_telp" />
+                                                            
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-md-2">
+                                                        <label class="font-weight-bolder">Jenis Kelamin<span
+                                                                class="required">*</span></label>
+                                                    </div>
+                                                    <div class="col-md-4 controls">
+                                                        <select diasbled class="select2 form-control" id="jenkel"  name="jenkel" readonly required>
+                                                            <option selected="" value="Pria">Pria</option>
+                                                            <option value="Wanita">Wanita</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-md-2">
+                                                        <label class="font-weight-bolder">Jabatan<span
+                                                                class="required">*</span></label>
+                                                    </div>
+                                                    <div class="col-md-4 controls">
+                                                        <select diasbled class="select2 form-control" id="id_jabatan"  name="id_jabatan"  required>
+                                                            <option selected="" value="">Pilih Jabatan ...</option>
+                                                            @foreach ($jabatan as $data)
+                                                            <option @if($data->id == $datapegawai->id_jabatan) selected @endif value="{{ $data->id }}">{{$data->nama_jabatan}}</option>
+                                                            
+                                                            @endforeach
+                                                        </select>
                                                     </div>
                                                 </div>
                                                 <div class="form-group row">
@@ -43,7 +100,7 @@
                                                                 class="required">*</span></label>
                                                     </div>
                                                     <div class="col-md-4 controls">
-                                                        <select class="select2 form-control" id="status"  name="status"  disabled required>
+                                                        <select diasbled  class="select2 form-control" id="status"  name="status" aria-readonly="true">
                                                             <option selected="" value="ACTIVE">ACTIVE</option>
                                                             <option value="INACTIVE">INACTIVE</option>
                                                         </select>
@@ -79,8 +136,9 @@
 @section('script')
     <script>
         $(document).ready(function() {
-            var dataupdate = {!! json_encode($datajabatan) !!};
+            var dataupdate = {!! json_encode($datapegawai) !!};
             $("#status select").val(dataupdate.status);
+            $("#jenkel select").val(dataupdate.jenkel);
             var date = new Date();
             var day = date.getDate();
             var month = date.getMonth() + 1;
@@ -90,6 +148,7 @@
             if (day < 10) day = "0" + day;
 
             var today = year + "-" + month + "-" + day;   
+            //$("#status").select2({disabled:'readonly'});
         });
        
        
@@ -118,7 +177,7 @@
 
             //console.log(pcin_tra_permintaan_pembelian);
             $.ajax({
-                url: "<?php echo url('/mas_data_jabatan/update/') ?>"+"/"+$("#id").val(),
+                url: "<?php echo url('/mas_data_pegawai/update');?>"+"/"+$("#id").val(),
                 type: "POST",
                 data: dataarray,
                 dataType: "JSON",
@@ -134,7 +193,7 @@
                             }
                         }).then((result) => {
                             if (result.isConfirmed) {
-                                window.location.href = "{{ route('mas_data_jabatan_index') }}";
+                                window.location.href = "{{ route('mas_data_pegawai') }}";
                             } else if (result.isDenied) {
                                 return false;
                             }
